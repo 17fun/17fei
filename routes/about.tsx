21ecf7efdd285e8002612cee3c0ebada399cc357
fun <jun@mankei.cc>
2024-04-11 06:01:28 +0800

    -

--- routes/about.tsx
+++ routes/about.tsx
@@ -1,29 +1,29 @@
 export default function About() {
-    return (
-        <div class="w-full p-2 leading-8 min-h-screen text-shadow bg-pink-300 text-lg text-red-100">
-            <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-                <div class="p-2 flex items-center w-full">
-                    <a class="flex-1" href="/">
-                        <img src="/logo.png" class="w-12 h-12" />
-                    </a>
-                </div>
-                <div class="leading-8 text-gray-900 max-w-lg mt-8 max-auto">
-                    <div class="text-lg text-gray-500">关于本站</div>
-                    <div class="my-4">网站目前只接收客服人工收款</div>
-                    <div>10元开通单月会员</div>
-                    <div>
-                        <span class="line-through">99</span>49元开通终身会员
-                    </div>
-                    <div>支付后添加微信[ wbot10 ] 1~12小时内响应</div>
-                    <div class="mt-4">
-                        如需其他定制开发， 欢迎联系合作。
-                        <a href="/custom" class="underline">如何制作一个相同的网站赚钱</a>
-                    </div>
-                </div>
-                <div>
-                    <img src="/qrcode.JPG" class="max-w-xs rounded-lg my-4" />
-                </div>
-            </div>
+  return (
+    <div class="text-shadow min-h-screen w-full bg-pink-300 p-2 text-lg leading-8 text-red-100">
+      <div class="mx-auto flex max-w-screen-md flex-col items-center justify-center">
+        <div class="flex w-full items-center p-2">
+          <a class="flex-1" href="/">
+            <img src="/logo.png" class="h-12 w-12" />
+          </a>
         </div>
-    );
+        <div class="max-auto mt-8 max-w-lg leading-8 text-gray-900">
+          <div class="text-lg text-gray-500">关于本站</div>
+          <div class="my-4">网站目前只接收客服人工收款</div>
+          <div>10元开通会员</div>
+          <div>99元网站部署服务（含素材，开源库中不含敏感素材）</div>
+          <div>支付后添加微信[ wbot10 ] 1~12小时内响应</div>
+          <div class="mt-4">
+            如需其他定制开发， 欢迎联系合作。
+            <a href="/custom" class="underline">
+              如何制作一个相同的网站赚钱
+            </a>
+          </div>
+        </div>
+        <div>
+          <img src="/qrcode.JPG" class="my-4 max-w-xs rounded-lg" />
+        </div>
+      </div>
+    </div>
+  );
 }
--- routes/custom.tsx
+++ routes/custom.tsx
@@ -1,38 +1,33 @@
 export default function Custom() {
-    return (
-        <div class="w-full p-2 leading-8 min-h-screen text-shadow bg-pink-300 text-lg text-red-100">
-            <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-                <div class="p-2 flex items-center w-full">
-                    <a class="flex-1" href="/">
-                        <img src="/logo.png" class="w-12 h-12" />
-                    </a>
-                </div>
-                <div class="leading-8 text-gray-900 max-w-lg mt-8 max-auto">
-                    <div class="text-lg text-gray-500">本网站怎么赚钱</div>
-                    <div class="my-4">
-                        这个网站其实不怎么赚钱，
-                        但是好处是不用太大成本。人人都可以玩。
-                        <div>
-                            网站纯静态，只要修改收款和客服信息和logo名字就可以拥有一个属于自己的情侣游戏网站。
-                        </div>
-                        如果你开通过会员，
-                        就知道人工收款过程，虽然对于用户体验不是很好，但是赚点小钱这样足够了。
-                        <p>
-                            网站目前每个月的收入大概在400~800左右，主要是小红书的笔记带来的流量。其实网站做好后我放置一段时间，因为小红书的笔记管理很严格，感觉没法玩。
-                            但直到一篇老笔记被官方翻牌，收获几十个红心。就这么一点点流量其实就可以带来每月几百块的收入了，近期我又偶尔更新一下笔记，但是效果很随机, 我还没有找到窍门吧。
-                        </p>
-                        如果你也想搭建一个，
-                        我可以把所有流程和代码发你，并且帮你上线部署。
-                        全程除了你自己的域名购买，
-                        没有二次收费。服务器都是用免费的就可以，不用备案，目前很稳。
-                        <p>网站主要有飞行棋和姿势卡两个变现的内容为主</p>
-                        <p>
-                            搭建一个收200元，会流量玩法的可以弄一下，我也偷师学习一下。
-                        </p>
-                        <p class="font-black">加微信 wbot10</p>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div class="text-shadow min-h-screen w-full bg-pink-300 p-2 text-lg leading-8 text-red-100">
+      <div class="mx-auto flex max-w-screen-md flex-col items-center justify-center">
+        <div class="flex w-full items-center p-2">
+          <a class="flex-1" href="/">
+            <img src="/logo.png" class="h-12 w-12" />
+          </a>
         </div>
-    );
+        <div class="max-auto mt-8 max-w-lg leading-8 text-gray-900">
+          <div class="text-lg text-gray-500">本网站怎么赚钱</div>
+          <div class="my-4">
+            这个网站其实不怎么赚钱， 但是好处是不用太大成本。人人都可以玩。
+            <div>
+              网站纯静态，只要修改收款和客服信息和logo名字就可以拥有一个属于自己的情侣游戏网站。
+            </div>
+            如果你开通过会员，
+            就知道人工收款过程，虽然对于用户体验不是很好，但是赚点小钱这样足够了。
+            <div>
+              如果你也想搭建一个，
+              我可以把所有流程和代码发你，并且帮你上线部署。
+              全程除了你自己的域名购买，
+              没有二次收费。服务器都是用免费的就可以，不用备案，目前很稳。
+            </div>
+            <p>网站主要有飞行棋和姿势卡两个变现的内容为主</p>
+            <p>搭建一个收99元，会流量SEO玩法的可以弄一下，我也偷师学习一下。</p>
+            <p class="font-black">加微信 wbot10</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
